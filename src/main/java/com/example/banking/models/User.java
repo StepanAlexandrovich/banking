53c1c6316168937769a160da3579edee8335572c
stepan <fisenko.stepan@mail.ru>
2023-10-21 13:42:47 +0300

    banking_0

---
+++ src/main/java/com/example/banking/models/User.java
@@ -0,0 +1,28 @@
+package com.example.banking.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "login")
+    private String login;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "name")
+    private String name;
+    @OneToMany(mappedBy = "user")
+    private List<Account> accounts = new ArrayList<>();
+
+}
